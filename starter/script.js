bbeb1eb3e3f813e5422130edf7a7016be322e086
AlonaAkimova <aliona.akimova@gmail.com>
2023-05-31 10:50:23 +0700

    changes

--- starter/script.js
+++ starter/script.js
@@ -66,7 +66,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // check if player`s score is >= 20
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
